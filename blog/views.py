37ecbf2e905e9b99721156d2d7bced64b2f9ff45
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-12-10 11:26:18 +0000

    add text readme add postmodels and views

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,9 @@ class PostList(generic.ListView):
     model = Post
     template_name = 'blog/post_list.html'  
     context_object_name = 'posts'  
-    ordering = ['-created_at']  
+    ordering = ['-created_on']  
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'  
+    context_object_name = 'post'
