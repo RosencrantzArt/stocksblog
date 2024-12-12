e2f28b3ace61c276137de80f71dccb3e5eda7f46
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-12-12 13:38:09 +0000

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -20,3 +20,26 @@ class PostDetail(generic.DetailView):
 
 def home(request):
     return render(request, 'home.html')
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
