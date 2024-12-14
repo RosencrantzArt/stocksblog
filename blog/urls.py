67cece0f98c543d2fe84f1f546cd4c5bdad78d02
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-12-14 06:05:25 +0000

    add about app and content

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='post_list'),  
     path('', views.home, name='home'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),  
+    path('about/', include('about.urls')),
 ]
 
