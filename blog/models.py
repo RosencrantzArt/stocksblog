3d3be4574a5d4371719549bc0dae7a5c796ccef7
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-12-17 18:21:38 +0000

    add posting, writing and comment

--- blog/models.py
+++ blog/models.py
@@ -24,9 +24,11 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+
